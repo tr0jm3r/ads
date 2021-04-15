30179421c5c636a8579ea0b4b91c3269ee4c9835
tr0jm3r <82596520+tr0jm3r@users.noreply.github.com>
2021-04-15 19:12:27 +0300

    Add files via upload

---
+++ bloom_filter.cpp
@@ -0,0 +1,70 @@
+esi-serial = &"serial"
+edi-temp = &""
+for i in serial:
+	temp[i] = serial[i]
+	eax = 0
+	edx(temp) = temp[0]
+	temp[0] = AL
+	ecx = 100
+	esi(&serial) = &name
+	edi = &temp
+	for
+		eax = 0
+		AL = name[0]
+		name = name[1]
+		if(eax == 0): break
+		eax >> 3
+		eax ^ 2C
+		eax << 4
+		ROR eax >> 10
+		eax ^ 26
+		eax += 40
+		eax >> 4
+		temp[i] = AL
+		
+		
+		
+		
+edi = "5\0"
+
+
+push "5\0"[1] // '\0'
+push "5\0"[0] // '5'
+push ebx // 1
+esi = name
+edi = temp
+edx = pa$$
+ecx = 1
+ebx = 45
+ebx << 8
+for i in ...
+	BL = 26
+	EAX = 0
+	----AL = name[i]
+	if(AL == 0): break
+	----i++
+	if(*edx == 0): edx = pa$$
+	----AL ^ pa$$[j]
+	----AL * BH
+	----AX = !AX
+	EAX && 00000fff
+	----AL = (AX)/BL, AH = (AX) % BL
+	----AH += 41
+	EAX << 10
+	----EAX >> 18    // AH = AL
+----push EAX
+	BL = 5
+	EAX = ECX (1)
+	EAX && 00000FFF
+	AH = (AL)%BL, AL = (AL)/BL
+	if(AH == 0):
+		temp[k] = 2D
+		k++
+----pop EAX
+	temp[k] = AL
+	k++
+	j++
+	ecx++
+
+JD39-CK4-5QV345
+
